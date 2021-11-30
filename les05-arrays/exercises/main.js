81e94f333595317de179313f5bd571cd409ad640
Korneel Eeckhout <korneel@pienter.tech>
2021-11-30 10:51:12 +0100

    les 5 arrays

--- les05-arrays/exercises/main.js
+++ les05-arrays/exercises/main.js
@@ -3,32 +3,82 @@
  * @param {array} anArray
  * @return {array}
  */
-function removeDoubles(anArray) {}
+function removeDoubles(anArray) {
+    let uniqueArr = [];
 
-console.log(removeDoubles([3, 5, 7, 12, 3, 6, 9, 10, 10, 5])); // [3, 5, 7, 12, 6, 9, 10]
+    for (let i = 0; i < anArray.length; i++) {
+        if (!uniqueArr.includes(anArray[i])) {
+            uniqueArr.push(anArray[i]);
+        }
+    }
+
+    return uniqueArr;
+}
 
 /**
  * Given an array of numbers, return the same array containing only even numbers
  * @param {number[]} anArray
  * @return {number[]}
  */
-function onlyEven(anArray) {}
+function onlyEven(anArray) {
+    let evenArray = [];
+
+    for (let i = 0; i < anArray.length; i++) {
+        if (anArray[i] % 2 === 0) {
+            evenArray.push(anArray[i]);
+        }
+    }
+
+    return evenArray;
+}
 
 /**
  * Sort an array from low to high, write the logic yourself
  * @param {number[]} anArray
  * @return {number[]}
  */
-function sort(anArray) {}
+function sort(anArray) {
+    let done = false;
+
+    while (!done) {
+        done = true;
+        for (let i = 0; i < anArray.length; i++) {
+            if (i + 1 < anArray.length && anArray[i] > anArray[i + 1]) {
+                done = false;
+                let tempVal = anArray[i];
+                anArray[i] = anArray[i + 1];
+                anArray[i + 1] = tempVal;
+            }
+        }
+    }
+
+    return anArray;
+}
 
 /**
  * Reverse the string
  * Try to keep the same casing (if first letter is
  * uppercase, make sure it is still uppercase in the reversed word
- * @param {string} aString
+ * @param {string} str
  * @return {string}
  */
-function reverseString(aString) {}
+function reverseString(str) {
+    let strArray = str.split('');
+
+    strArray.reverse();
+
+    for (let i = 0; i < strArray.length; i++) {
+        if (str[i] === str[i].toUpperCase()) {
+            strArray[i] = strArray[i].toUpperCase();
+        } else {
+            strArray[i] = strArray[i].toLowerCase();
+        }
+    }
+
+    return strArray.join('');
+
+    // return str.split('').reverse().join('');
+}
 
 /**
  * Given two arrays of values, return an array with only the values that occur in both arrays
@@ -36,13 +86,46 @@ function reverseString(aString) {}
  * @param {array} otherArray
  * @return {array}
  */
-function findSimilars(anArray, otherArray) {}
+function findSimilars(anArray, otherArray) {
+    let similarArray = [];
+
+    for (let i = 0; i < anArray.length; i++) {
+        let otherArrayIndex = otherArray.indexOf(anArray[i]);
+        if (otherArrayIndex > -1) {
+            similarArray.push(anArray[i]);
+            otherArray.splice(otherArrayIndex, 1);
+        }
+    }
+
+    return similarArray;
+}
 
 /**
  * Given an array return an array with the same values but in a random order
  * @param anArray
  */
-function scramble(anArray) {}
+function scramble(anArray) {
+    // for (let i = 0; i < anArray.length; i++) {
+    //     let randomIndex = Math.floor(Math.random() * anArray.length);
+    //
+    //     let tempVal = anArray[i];
+    //     anArray[i] = anArray[randomIndex];
+    //     anArray[randomIndex] = tempVal;
+    // }
+    //
+    // return anArray;
+
+    let scrambledArray = [];
+
+    while (anArray.length) {
+        let randomIndex = Math.floor(Math.random() * anArray.length);
+
+        let randomVal = anArray.splice(randomIndex, 1);
+        scrambledArray.push(randomVal[0]);
+    }
+
+    return scrambledArray;
+}
 
 /**
  * Given two arrays, check if the second array (subArray) is a sublist of the first (bigArray)
@@ -50,4 +133,34 @@ function scramble(anArray) {}
  * @param {array} subArray
  * @return {boolean}
  */
-function isSublist(bigArray, subArray) {}
+function isSublist(bigArray, subArray) {
+    let isSubArray = false;
+
+    for (let i = 0; i < bigArray.length; i++) {
+        console.log(bigArray[i]);
+        if (bigArray[i] === subArray[0]) {
+            console.log('found first item');
+            isSubArray = true;
+
+            for (let j = 0; j < subArray.length; j++) {
+                console.log(subArray[j]);
+                console.log(bigArray[i + j]);
+
+                if (subArray[j] !== bigArray[i + j]) {
+                    console.log('wrong val');
+                    isSubArray = false;
+                    break;
+                }
+            }
+        }
+
+        if (isSubArray) {
+            // return true;
+            break;
+        }
+    }
+
+    // return false;
+    console.log('return ' + isSubArray);
+    return isSubArray;
+}
