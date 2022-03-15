86bc980bfb64b45aff02ca8edd111f5fe0375e1a
Korneel Eeckhout <korneel@pienter.tech>
2022-03-15 14:15:36 +0100

    les 18 - quiz

--- les18-fetch/quiz/main.js
+++ les18-fetch/quiz/main.js
@@ -4,13 +4,35 @@ let $catSelectSelect = document.getElementById('cat-select-select');
 let $catSelectSubmit = document.getElementById('cat-select-submit');
 let $quizContainer = document.getElementById('quiz-container');
 let $quizTitle = document.getElementById('quiz-title');
+let $quizAnswerContainer = document.getElementById('quiz-answer-container');
+let $quizQuestionNumbers = document.querySelectorAll('.quiz__question-number');
+let $quizScore = document.getElementById('quiz-score');
+let $quizQuestion = document.getElementById('quiz-question');
+
+const QuestionCount = 10;
 
 let state = {
+    error: false,
+    catFetchComplete: false,
+    gameOver: false,
     selectedCatId: null,
     selectCatText: null,
+    score: 0,
+    turn: 0,
+    questions: null,
+    token: null,
 };
 
 // Draw Function
+
+/**
+ *
+ * @param {number} index
+ * @param {'active' | 'correct' | 'wrong'} type
+ */
+function drawQuestionNumberClass(index, type) {
+    $quizQuestionNumbers[index].classList.add(`quiz__question-number--${type}`);
+}
 
 function drawPage() {
     if (state.selectedCatId === null && state.selectCatText === null) {
@@ -46,16 +68,57 @@ function drawOptions(categories) {
     $catSelectSelect.innerHTML = optionsHTML;
 }
 
+function resetQuestionNumberClasses() {
+    $quizQuestionNumbers.forEach(($quizQuestionNumber) => {
+        $quizQuestionNumber.classList.remove(
+            'quiz__question-number--active',
+            'quiz__question-number--correct',
+            'quiz__question-number--wrong',
+        );
+    });
+}
+
+function drawScore() {
+    $quizScore.innerText = state.score;
+}
+
+function drawQuestion() {
+    if (state.questions === null) {
+        $quizQuestion.innerHTML = '';
+        return;
+    }
+
+    let curQuestion = state.questions[state.turn];
+
+    $quizQuestion.innerHTML = curQuestion.question;
+}
+
+function drawEndQuestion() {
+    $quizQuestion.innerHTML = `Quiz over, you scored ${state.score} points. Try again?`;
+}
+
+function drawErrorQuestion() {
+    $quizQuestion.innerHTML = `Could not find questions for ${state.selectCatText}. Select a different category?`;
+}
+
+function removeCatOption(catId) {
+    let $option = document.querySelector(
+        `.cat-select__select option[value="${catId}"]`,
+    );
+    $option.remove();
+}
+
 // Fetch Functions
 
 function fetchQuestions() {
     let queryParams = new URLSearchParams();
-    queryParams.append('amount', 10);
+    queryParams.append('amount', QuestionCount);
     queryParams.append('category', state.selectedCatId);
     queryParams.append('difficulty', 'medium');
     queryParams.append('type', 'boolean');
+    queryParams.append('token', state.token);
 
-    fetch('https://opentdb.com/api.php?' + queryParams.toString())
+    return fetch('https://opentdb.com/api.php?' + queryParams.toString())
         .then((response) => {
             if (!response.ok) {
                 throw new Error('Could not fetch questions');
@@ -64,7 +127,7 @@ function fetchQuestions() {
             return response.json();
         })
         .then((body) => {
-            console.log(body);
+            return body;
         })
         .catch((error) => {
             console.error(error);
@@ -72,9 +135,7 @@ function fetchQuestions() {
 }
 
 function fetchCategories() {
-    disableCatForm();
-
-    fetch('https://opentdb.com/api_category.php')
+    return fetch('https://opentdb.com/api_category.php')
         .then((response) => {
             if (!response.ok) {
                 throw new Error('Could not fetch categories');
@@ -83,14 +144,118 @@ function fetchCategories() {
             return response.json();
         })
         .then((body) => {
-            drawOptions(body.trivia_categories);
+            return body.trivia_categories;
         })
         .catch((error) => {
             console.error(error);
-        })
-        .finally(() => {
-            enableCatForm();
         });
+}
+
+function fetchToken() {
+    return fetch('https://opentdb.com/api_token.php?command=request')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Could not fetch token');
+            }
+
+            return response.json();
+        })
+        .then((body) => {
+            return body.token;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+// Game functions
+
+function initCat() {
+    state.selectedCatId = null;
+    state.selectCatText = null;
+
+    if (!state.catFetchComplete) {
+        disableCatForm();
+
+        Promise.all([fetchToken(), fetchCategories()])
+            .then((bodies) => {
+                let token = bodies[0];
+                let categories = bodies[1];
+                drawOptions(categories);
+                state.catFetchComplete = true;
+                state.token = token;
+            })
+            .finally(() => {
+                enableCatForm();
+            });
+    }
+
+    drawPage();
+}
+
+function initQuiz() {
+    state.score = 0;
+    state.turn = 0;
+    state.questions = null;
+    state.gameOver = false;
+    state.error = false;
+
+    fetchQuestions().then((body) => {
+        if (body.response_code !== 0) {
+            drawErrorQuestion();
+            state.error = true;
+            removeCatOption(state.selectedCatId);
+            return;
+        }
+
+        state.questions = body.results;
+        drawQuestion();
+    });
+
+    resetQuestionNumberClasses();
+    drawScore();
+    drawQuestion();
+    drawQuestionNumberClass(state.turn, 'active');
+}
+
+/**
+ * Handle answer
+ * @param {'True'|'False'} curAnswer
+ */
+function answer(curAnswer) {
+    if (state.error) {
+        initCat();
+        return;
+    }
+
+    if (state.gameOver) {
+        console.log(curAnswer);
+        if (curAnswer === 'True') {
+            initQuiz();
+        } else {
+            initCat();
+        }
+        return;
+    }
+
+    let correctAnswer = state.questions[state.turn].correct_answer;
+
+    if (curAnswer === correctAnswer) {
+        state.score++;
+        drawScore();
+        drawQuestionNumberClass(state.turn, 'correct');
+    } else {
+        drawQuestionNumberClass(state.turn, 'wrong');
+    }
+
+    if (state.turn === QuestionCount - 1) {
+        drawEndQuestion();
+        state.gameOver = true;
+    } else {
+        state.turn++;
+        drawQuestionNumberClass(state.turn, 'active');
+        drawQuestion();
+    }
 }
 
 // Event Handlers
@@ -98,15 +263,24 @@ function fetchCategories() {
 function submitCatSelect(event) {
     event.preventDefault();
 
-    let activeOption = event.target.querySelector('option:checked');
+    let activeOption = event.target.querySelector('.cat-select__select option:checked');
 
     state.selectedCatId = activeOption.value;
     state.selectCatText = activeOption.innerText;
 
-    fetchQuestions();
     drawPage();
+
+    initQuiz();
+}
+
+function answerBtnClicked(event) {
+    // execute answer with correct parameter
+    if (event.target.matches('.quiz__answer')) {
+        answer(event.target.innerText);
+    }
 }
 
 $catSelectForm.addEventListener('submit', submitCatSelect);
+$quizAnswerContainer.addEventListener('click', answerBtnClicked);
 
-fetchCategories();
+initCat();
