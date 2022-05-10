a9692f79403a368ab760fe1a523bae6d0eac9b15
Korneel Eeckhout <korneel@pienter.tech>
2022-05-10 12:13:24 +0200

    les23 - vue quiz

---
+++ les25-vue-quiz/vue-quiz/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <CatSelect v-if="showCatSelect" @selectCat="selectCat" />
+
+    <Quiz v-else :cat-data="catData" @end-quiz="endQuiz" />
+</template>
+
+<script>
+import CatSelect from './components/CatSelect.vue';
+import Quiz from './components/Quiz.vue';
+
+export default {
+    components: {
+        Quiz,
+        CatSelect,
+    },
+    data() {
+        return {
+            showCatSelect: true,
+            catData: null,
+        };
+    },
+    computed: {},
+    methods: {
+        selectCat(catData) {
+            console.log(catData);
+            this.showCatSelect = false;
+            this.catData = catData;
+        },
+        endQuiz() {
+            this.showCatSelect = true;
+        },
+    },
+    created() {},
+};
+</script>
+
+<style>
+@import './assets/style.css';
+</style>
